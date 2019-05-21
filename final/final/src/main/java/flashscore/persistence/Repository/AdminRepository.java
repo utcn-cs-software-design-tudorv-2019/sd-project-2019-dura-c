f0bfb0ca5b2a73e9c82736878bfef000d715f492
DuraCatalin <catalindura97@yahoo.com>
2019-05-21 11:31:36 +0300

    remove

--- final/final/src/main/java/flashscore/persistence/Repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package flashscore.persistence.Repository;
-
-import flashscore.persistence.Entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin,Integer> {
-    public Admin findByUsername(String username);
-    public Admin save(Admin admin);
-}
